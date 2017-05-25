a112246300ef9b6d50ed505090de9481eb3b8360
rchen2 <rodolfo_chen@intuit.com>
2017-05-25 10:15:20 -0700

    first commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var meowImg: UIImageView!
+    @IBOutlet weak var helloBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func hiButtonAction(_ sender: Any) {
+        bgImg.isHidden = false
+        meowImg.isHidden = false
+        helloBtn.isHidden = true
+    }
 }
 
